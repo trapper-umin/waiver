16638f6c21432d2d263c93ab5ff27af1907146d8
Artem Krumin <trapper.programmer@gmail.com>
2023-08-10 14:32:24 +0300

    configuration file was replace on YAML format

--- src/main/java/dev/waiver/com/models/Person.java
+++ src/main/java/dev/waiver/com/models/Person.java
@@ -2,10 +2,7 @@ package dev.waiver.com.models;
 
 import dev.waiver.com.models.common.AbstractEntity;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
